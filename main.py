d9dd5f356b1f8833e7ac057792b9d87ff82694b8
Gaia Negev <40687916-gaianegev-star@users.noreply.replit.com>
2025-03-28 12:11:23 +0000

    Restored to '063587a0bc4ffb4882e10d0abfd241e4552e93c6'
    Replit-Restored-To: 063587a0bc4ffb4882e10d0abfd241e4552e93c6

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ charms_all_students = [["Ron", "Harry", "Hermione", "Neville", "Draco"],
 divination_all_students = [["Ron", "Harry", "Hermione", "Draco"],
                            [60, 70, 100, 60], [95, 95, 100, 65]]
 
-print("\nQuestion 2 results:")
 print(
   compare_subjects_within_student(
       charms_all_students,
@@ -41,5 +40,4 @@ print(
 # Question 3 #
 ##############
 
-print("\nQuestion 3 results:")
 complicated_palindrome()
